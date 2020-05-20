cea828e78560894fbc4696b03866c06aec4ac36b
FrancescoSaro <munitsaro@gmail.com>
2020-05-20 14:06:18 +0200

    BT17 GetPlayerTank

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -2,22 +2,34 @@
 
 
 #include "TankAIController.h"
+#include "Engine/World.h"
+#include "..\Public\TankAIController.h"
 
 void ATankAIController::BeginPlay()
 {
     Super::BeginPlay();
-    auto ControlledTank = GetControlledTank();
-    if (!ControlledTank)
+    auto PlayerTank = GetPlayerTank();
+    if (!PlayerTank)
     {
         UE_LOG(LogTemp, Error, TEXT("Miss AIControlledTank "));
     }
     else
     {
-        UE_LOG(LogTemp, Warning, TEXT("AIController possessing:  %s"), *(ControlledTank->GetName()));
+        UE_LOG(LogTemp, Warning, TEXT("AIController possessing:  %s"), *(PlayerTank->GetName()));
     }
 }
 
 ATank* ATankAIController::GetControlledTank() const
 {
     return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+    auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
+    if (!PlayerTank)
+    {
+        return nullptr;
+    }
+    return Cast<ATank>(PlayerTank);
 }
